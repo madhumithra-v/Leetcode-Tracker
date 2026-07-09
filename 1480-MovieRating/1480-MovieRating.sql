1cdc5c22638c2d3fe64b0441ac3b32e861be2af4
MADHUMITHRA VELUSAMY <madhumithra261409@gmail.com>
2026-07-09 15:06:51 +0530

    Create 1480-MovieRating

---
+++ 1480-MovieRating/1480-MovieRating.sql
@@ -0,0 +1,23 @@
+-- Last updated: 7/9/2026, 3:06:47 PM
+(
+SELECT u.name AS results
+FROM MovieRating mr
+JOIN Users u
+ON mr.user_id=u.user_id
+GROUP BY u.user_id,u.name
+ORDER BY COUNT(*) DESC,u.name
+LIMIT 1
+)
+
+UNION ALL
+
+(
+SELECT m.title
+FROM MovieRating mr
+JOIN Movies m
+ON mr.movie_id=m.movie_id
+WHERE DATE_FORMAT(created_at,'%Y-%m')='2020-02'
+GROUP BY m.movie_id,m.title
+ORDER BY AVG(rating) DESC,m.title
+LIMIT 1
+);
