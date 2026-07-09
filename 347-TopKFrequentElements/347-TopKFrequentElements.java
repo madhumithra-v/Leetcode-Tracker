4c95b6e1a1d9c5da3efbad59c920d1a770cee79a
MADHUMITHRA VELUSAMY <madhumithra261409@gmail.com>
2026-07-09 15:07:47 +0530

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 3:07:43 PM
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(b) - map.get(a)
+        );
+
+        pq.addAll(map.keySet());
+
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll();
+        }
+
+        return result;
+    }
+}
