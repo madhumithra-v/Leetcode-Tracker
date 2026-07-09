ef3dc66a2f21bae17a21496a905a30da69ef630e
MADHUMITHRA VELUSAMY <madhumithra261409@gmail.com>
2026-07-09 15:08:24 +0530

    Create 164-MaximumGap

---
+++ 164-MaximumGap/164-MaximumGap.java
@@ -0,0 +1,51 @@
+// Last updated: 7/9/2026, 3:08:20 PM
+class Solution {
+    public int maximumGap(int[] nums) {
+
+        if (nums == null || nums.length < 2)
+            return 0;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        if (min == max)
+            return 0;
+
+        int n = nums.length;
+        int bucketSize = Math.max(1, (max - min) / (n - 1));
+        int bucketCount = (max - min) / bucketSize + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+        boolean[] used = new boolean[bucketCount];
+
+        for (int i = 0; i < bucketCount; i++) {
+            bucketMin[i] = Integer.MAX_VALUE;
+            bucketMax[i] = Integer.MIN_VALUE;
+        }
+
+        for (int num : nums) {
+            int idx = (num - min) / bucketSize;
+            bucketMin[idx] = Math.min(bucketMin[idx], num);
+            bucketMax[idx] = Math.max(bucketMax[idx], num);
+            used[idx] = true;
+        }
+
+        int prev = min;
+        int ans = 0;
+
+        for (int i = 0; i < bucketCount; i++) {
+            if (!used[i]) continue;
+
+            ans = Math.max(ans, bucketMin[i] - prev);
+            prev = bucketMax[i];
+        }
+
+        return ans;
+    }
+}
