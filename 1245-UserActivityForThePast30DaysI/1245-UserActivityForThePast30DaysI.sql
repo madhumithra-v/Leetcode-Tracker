3617346bdd74f07062f69094570cc70edf204f76
MADHUMITHRA VELUSAMY <madhumithra261409@gmail.com>
2026-07-09 15:07:08 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,8 @@
+-- Last updated: 7/9/2026, 3:07:04 PM
+SELECT
+activity_date AS day,
+COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28'
+AND '2019-07-27'
+GROUP BY activity_date;
